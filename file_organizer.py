7f6f451ae4ef2bf5937864e1dcc5133f3bb72328
Joey-Gordon-Dev <149360765+Joey-Gordon-Dev@users.noreply.github.com>
2023-11-20 12:29:14 -0800

    Update file_organizer.py

--- file_organizer.py
+++ file_organizer.py
@@ -1,25 +1,30 @@
 import os
 import shutil
 
-current_dir = os.getcwd()
-videos_folder = current_dir + "/Videos"
-pictures_folder = current_dir + "/Pictures"
-files_in_dir = os.listdir()
+try:
+    current_dir = os.getcwd()
+    videos_folder = current_dir + "/Videos"
+    pictures_folder = current_dir + "/Pictures"
+    docs_folder = current_dir + "/Documents"
+    files_in_dir = os.listdir()
 
-length_of_files_in_dir = len(files_in_dir)
-
-os.makedirs(pictures_folder)
-os.makedirs(videos_folder)
-
-
-def main():
-    for i in range(length_of_files_in_dir):
-        # adding picture files to picture folder
-        if files_in_dir[i].endswith(".png") or files_in_dir[i].endswith(".jpeg") or files_in_dir[i].endswith(".jpg"):
-            shutil.move(current_dir + "/" + files_in_dir[i], pictures_folder)
-        # adding video files to video folder
-        if files_in_dir[i].endswith(".mp3") or files_in_dir[i].endswith(".wav") or files_in_dir[i].endswith(".mp4") or files_in_dir[i].endswith(".mov") or files_in_dir[i].endswith(".mkv"):
-            shutil.move(current_dir + "/" + files_in_dir[i], videos_folder)
+    length_of_files_in_dir = len(files_in_dir)
 
 
-main()
+    def main():
+        for i in range(length_of_files_in_dir):
+            if files_in_dir[i].endswith(".png") or files_in_dir[i].endswith(".jpeg") or files_in_dir[i].endswith(".jpg"):
+                os.makedirs(pictures_folder)
+                shutil.move(current_dir + "/" + files_in_dir[i], pictures_folder)
+            if files_in_dir[i].endswith(".mp3") or files_in_dir[i].endswith(".wav") or files_in_dir[i].endswith(".mp4") or files_in_dir[i].endswith(".mov") or files_in_dir[i].endswith(".mkv"):
+                os.makedirs(videos_folder)
+                shutil.move(current_dir + "/" + files_in_dir[i], videos_folder)
+            if files_in_dir[i].endswith(".pdf") or files_in_dir[i].endswith(".ppt") or files_in_dir[i].endswith(".xls") or files_in_dir[i].endswith(".doc"):
+                os.makedirs(docs_folder)
+                shutil.move(current_dir + "/" + files_in_dir[i], docs_folder)
+
+
+    main()
+except:
+    print("Error: folder already exists.")
+    ValueError
